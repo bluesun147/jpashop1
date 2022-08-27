e6e157000c59553938df4510bb93b6b97b3eace6
HaeChan Kim <bluesun147@naver.com>
2022-08-27 21:31:03 +0900

    View 환경 설정

---
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) { // model에 어떤 데이터를 실어서 뷰에 넘김
+        model.addAttribute("data", "hello!!");
+        return "hello"; // hello.html
+    }
+}
