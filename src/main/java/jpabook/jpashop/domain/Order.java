93b72e88f0e4337a14cfcab3cd77115194b8e2b7
HaeChan Kim <bluesun147@naver.com>
2022-08-30 22:56:45 +0900

    엔티티 설계시 주의점

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -17,14 +17,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY) // 모든 연관관계 lazy로 할것, eager로 하면 모두 다 불러옴
     @JoinColumn(name = "member_id") // 매핑을 뭘로 할건지. fk이름이 member_id가 됨.
     private Member member; // 얘를 연관관계 주인. fk쪽을 주인으로.
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // order만 persist 하면 밑에것들도 다 한꺼번에.
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id") // 연관관계 주인
     private Delivery delivery;
 
@@ -33,4 +33,19 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OderStatus status; // 주문 상태 [ORDER, CANCEL]
 
+    // 연관관계 편의 메서드
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this); // 멤버 세팅 시 양방향 연관관계 알아서 해줌
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
