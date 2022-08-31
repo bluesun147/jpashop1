0391ae41493a57859129aba0d1e5ecb6dd10b293
HaeChan Kim <bluesun147@naver.com>
2022-08-31 20:44:49 +0900

    회원 기능 테스트

--- src/main/java/jpabook/jpashop/domain/MemberRepository.java
+++ src/main/java/jpabook/jpashop/domain/MemberRepository.java
@@ -1,24 +1,24 @@
-package jpabook.jpashop.domain;
-
-import org.springframework.stereotype.Repository;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-@Repository
-public class MemberRepository {
-
-    @PersistenceContext // 엔티티 매니저 사용 위해.
-    private EntityManager em;
-
-    // 저장
-    public Long save(Member member) {
-        em.persist(member); // 저장
-        return member.getId();
-    }
-
-    // 조회
-    public Member find(Long id) {
-        return em.find(Member.class, id);
-    }
-}
+//package jpabook.jpashop.domain;
+//
+//import org.springframework.stereotype.Repository;
+//
+//import javax.persistence.EntityManager;
+//import javax.persistence.PersistenceContext;
+//
+//@Repository
+//public class MemberRepository {
+//
+//    @PersistenceContext // 엔티티 매니저 사용 위해.
+//    private EntityManager em;
+//
+//    // 저장
+//    public Long save(Member member) {
+//        em.persist(member); // 저장
+//        return member.getId();
+//    }
+//
+//    // 조회
+//    public Member find(Long id) {
+//        return em.find(Member.class, id);
+//    }
+//}
