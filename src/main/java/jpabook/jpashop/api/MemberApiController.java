6ee3f201ce401f14698fc83b51b2e838a3ec64e8
HaeChan Kim <bluesun147@naver.com>
2022-09-13 22:30:32 +0900

    회원 조회 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,12 +8,53 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController // Controller와 ResponseBody 합친것. ResponseBody는 템플릿 아니고 데이터 자체(json등) 바로 리턴하는.
 @RequiredArgsConstructor
 public class MemberApiController {
     private final MemberService memberService;
 
+    // 회원 조회
+    // 가장 단순, 안좋은 버전 (엔티티 직접 노출하지 말자!)
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() { // 리스트로 바로 넘기면 확장 안좋음. json 형태로 넘기는게 좋다.
+        return memberService.findMembers();
+    }
+
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2() { // 응답값 껍데기 클래스 Result
+        List<Member> findMembers = memberService.findMembers(); // 가져와서 memberDto로 변환
+
+        // 멤버 엔티티에서 이름 꺼내와서 dto로 넣고 변환
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());// 리스트로 변환
+
+        // -> list Member를 list MemberDto로 변환
+
+        return new Result(collect);
+        // return new Result(collect.size(), collect); // count 넣는 경우
+    }
+
+    @Data
+    @AllArgsConstructor
+    // 오브젝트 타입으로 반환하기 때문에 Result라는 껍데기 씌워 줌.
+    // 이런식으로 한번 감싸줘야 한다. 리스트를 바로 내면 json 배열 타입으로 나가버리기 때문에 유연성 떨어짐.
+    static class Result<T> { // 제네릭
+        // private int count; // 이런식으로 바로 넣으면 됨.
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto { // 이름만 반환하는 api 만든다고 했을 때
+        private String name;
+    }
+
+    // 회원 등록
     // 인자로 엔티티 받지말자
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) { // 회원 엔티티 받음
@@ -72,3 +113,5 @@ public class MemberApiController {
     }
 
 }
+
+
